c25db7d8ab8152f63b5289ee64a17956d0eebef8
TheRageWay <kotyg1995@gmail.com>
2021-08-19 16:17:30 -0400

    Added intents

--- dkp.py
+++ dkp.py
@@ -13,7 +13,10 @@ import discord
 import logging
 
 #Variables
-client = discord.Client() 
+intents = discord.Intents(messages=True, guilds=True)
+intents.members = True
+intents.reactions = True
+client = discord.Client(intents=intents) 
 token = read_token()
 logger = logging.getLogger('discord')
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
@@ -38,17 +41,26 @@ async def on_ready():
 async def on_disconnect():
         print('DKP bot has disconnected from discord')
 
+#on_member_join
+@client.event
+async def on_member_join(member):
+        for channel in member.guild.channels:
+                if str(channel) == 'general':
+                        await channel.send(f'Welcome to the Warband {member.mention}')
+
 #on_message
 @client.event
 async def on_message(message):
+
         id = client.get_guild(877528142909161572)
-        if message.author == client.user:
-                return 
+        channels = ['dkp']
 
-        if message.content.startswith('$hello'):
-                await message.channel.send ('hello!')
+        if str(message.channel) in channels:
 
-        if message.content == "$users":
-                await message.channel.send(f"# of Users: {id.member_count}")
+                if message.content.startswith('$hello'):
+                        await message.channel.send('hello!')
+
+                if message.content == '$users':
+                        await message.channel.send(f'# of Users: {id.member_count}')
 
 client.run(token)
